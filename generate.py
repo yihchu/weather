adcab957a74a8eb7a886d3e3950e6dbb19b512a6
fu.qiang <fu.qiang@linecorp.com>
2019-12-25 14:26:26 +0800

    #

---
+++ generate.py
@@ -0,0 +1,11 @@
+import fileinput
+import sys
+import base64
+
+b64=base64.encodestring(open("./Dalian.png","rb").read())
+baseUrl="data:image/png;base64," + str(b64,'utf-8')
+
+for line in fileinput.input("result.html", inplace=1):
+    line=line.replace("YYYXXXWWW", sys.argv[1])
+    line=line.replace("WWWXXXYYY", baseUrl)
+    print(line,end="")
--- replace.py
+++ replace.py
@@ -6,7 +6,7 @@ for line in fileinput.input("result.html", inplace=1):
     if 'head' in line:
         line=line 
     else:
-        line=line.replace("天氣預報： Dalian",sys.argv[1])
+        line=line.replace("YYYXXXWWW",sys.argv[1])
     line=line.replace("body class=\"\"", "body bgcolor=\"#FFFAF0\"")
     line=line.replace("ffff00", "CD6600")
     print(line,end="")
