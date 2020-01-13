413236d26f879531b221887a4d9d84806328e9a3
fu.qiang <fu.qiang@linecorp.com>
2020-01-13 17:31:07 +0800

    #

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ author=$(echo $json | python3 -c "import sys, json; print(json.load(sys.stdin)['
 content=$(echo $json | python3 -c "import sys, json; print(json.load(sys.stdin)['data']['content'])")
 translate=$(echo $json | python3 -c "import sys, json; print(json.load(sys.stdin)['data']['translation'])")
 # result=$(echo $content  --$author <br/><br/>天氣預報： 大连)
-result=$(echo "<pre style='color:#111111'><br/><span style='font-family: STHeiti;font-size: 20'>早安~ 越来越调皮的吴小妍~ </span><br/><br/><HR style='FILTER: alpha(opacity=100,finishopacity=0,style=3)' color=#987cb9 SIZE=3><br/><span style='font-family: Helvetica;font-size: 18'>$content<br/><br/>$translate<br/><br/>---$author</span><br/><br/><br/><HR align=center color=#987cb9 SIZE=2>$oneLine</pre>")
+result=$(echo "<pre style='color:#111111'><br/><span style='font-family: STHeiti;font-size: 20'>早安~ 总是说不知道的吴小妍~ </span><br/><br/><HR style='FILTER: alpha(opacity=100,finishopacity=0,style=3)' color=#987cb9 SIZE=3><br/><span style='font-family: Helvetica;font-size: 18'>$content<br/><br/>$translate<br/><br/>---$author</span><br/><br/><br/><HR align=center color=#987cb9 SIZE=2>$oneLine</pre>")
 
 echo $result
 
